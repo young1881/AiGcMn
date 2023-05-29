c0891743c66aea49154681ff3086f69f33e32494
Sky <sky0722@sjtu.edu.cn>
2023-05-29 19:24:58 +0800

    以近半概率、极低效率生成对抗样本

--- aigcmn.py
+++ aigcmn.py
@@ -4,13 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
-from gendis import Generator
+from gendis import Generator, Discriminator
 
 
 class AiGcMn:
     def __init__(self):
         self.generator = Generator(110, 3136)
         self.generator.load_state_dict(torch.load(r'./model/Generator_cuda_32.pkl'))
+        self.discriminator = Discriminator()
+        self.discriminator.load_state_dict(torch.load(r'./model/Discriminator_cuda_32.pkl'))
         self.batchsize = 0
 
     def input(self):
@@ -25,10 +27,11 @@ class AiGcMn:
         return num_lst
 
     def showall(self, images):  # images=self.generator
-
         images = images.detach().numpy()
-        images = 255 * (0.5 * images + 0.5)
+        # images = 255 * (0.5 * images + 0.5)
+        images = 255 * images
         images = images.astype(np.uint8)
+        # print(images)
         plt.figure(figsize=(self.numno, self.numno))
         width = images.shape[2]
         gs = gridspec.GridSpec(1, self.numno, wspace=0, hspace=0)
@@ -42,7 +45,29 @@ class AiGcMn:
             plt.tight_layout()
         plt.tight_layout()
         return width
-
+    
+    def ad(self, img): # img是生成器的输出
+        epsilon = 0.02
+        noise = torch.zeros([1, 1, 28, 28])
+        for i in range(28):
+            for j in range(28):
+                loss_0 = torch.max(self.discriminator(img + noise))
+                if img[0, 0, i, j] + epsilon >= 0 and img[0, 0, i, j] + epsilon <= 1:
+                    noise[0, 0, i, j] += epsilon
+                    loss_1 = torch.max(self.discriminator(img + noise))
+                    if loss_1 <= loss_0:
+                        continue
+                    else:
+                        noise[0, 0, i, j] -= epsilon
+                        if img[0, 0, i, j] - epsilon >= 0 and img[0, 0, i, j] - epsilon <= 1:
+                            noise[0, 0, i, j] -= epsilon
+                            loss_2 = torch.max(self.discriminator(img + noise))
+                            if loss_2 <= loss_0:
+                                continue
+                            else:
+                                noise[0, 0, i, j] += epsilon
+        return img + noise
+    
     def generate(self, input) -> torch.tensor:
         num_img = 1
         piclist = []
@@ -54,7 +79,13 @@ class AiGcMn:
             finaltensor = np.concatenate((noisetensor.numpy(), numtensor), 1)
             finaltensor = torch.from_numpy(finaltensor).float()
             one_img = self.generator(finaltensor)  # 将向量放入生成网络G生成一张图片
-            piclist.append(one_img)
+            # 归一化：严格限制在[0, 1]
+            one_img = (one_img - one_img.min()) / (one_img.max() - one_img.min())
+            # print(one_img.size())
+            ad = self.ad(one_img)
+            print(torch.max(self.discriminator(ad)))
+            piclist.append(ad) # 使用对抗样本作为输出
+            # piclist.append(one_img)
         total_img = torch.cat(piclist, 0)
         self.showall(total_img)
         if not os.path.exists('./result/'):
